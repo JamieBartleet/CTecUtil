fd220d29a2bbf742c4aa9c706be915397042d247
JamieBartleet <jamie.bartleet@gmail.com>
2021-08-20 16:45:06 +0100

    210820 save
    210820 save

---
+++ Cursor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace CTecUtil
+{
+    /// <summary>
+    ///   Contains helper methods for UI, so far just one for showing a waitcursor
+    /// </summary>
+    public static class Cursor
+    {
+        /// <summary>
+        ///   A value indicating whether the UI is currently busy
+        /// </summary>
+        private static bool _isBusy;
+
+        /// <summary>
+        /// Sets the busystate as busy.
+        /// </summary>
+        public static void SetBusyState()
+        {
+            setBusyState(true);
+        }
+
+        /// <summary>
+        /// Sets the busystate to busy or not busy.
+        /// </summary>
+        /// <param name="busy">if set to <c>true</c> the application is now busy.</param>
+        private static void setBusyState(bool busy)
+        {
+            if (busy != _isBusy)
+            {
+                Mouse.OverrideCursor = (_isBusy = busy) ? Cursors.Wait : null;
+
+                if (_isBusy)
+                    new DispatcherTimer(TimeSpan.FromSeconds(0), DispatcherPriority.ApplicationIdle, dispatcherTimer_Tick, System.Windows.Application.Current.Dispatcher);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Tick event of the dispatcherTimer control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private static void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            var dispatcherTimer = sender as DispatcherTimer;
+            if (dispatcherTimer != null)
+            {
+                setBusyState(false);
+                dispatcherTimer.Stop();
+            }
+        }
+    }
+}
---
+++ Registry.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTecUtil
+{
+    public class Registry
+    {
+        /// <summary>
+        /// Initialise the CTecUtil.Registry class.
+        /// </summary>
+        /// <param name="productName">The software's name ("QuantecTools", XFPTools", etc.)</param>
+        public static void Initialise(string productName)
+        {
+            _productName = productName;
+            _initialised = true;
+        }
+
+
+        private static bool   _initialised;
+        private static string _productName;
+        private const  string _rootKey = @"SOFTWARE\CTec\";
+
+
+        /// <summary>
+        /// Read registry entry from HKEY_CURRENT_USER.
+        /// </summary>
+        /// <param name="subkeyName"></param>
+        public static object ReadSubKey(string subkeyName)
+        {
+            if (!_initialised) throw new Exception("CTecUtil: the Registry class has not been initialised.");
+
+            try
+            {
+                RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(_rootKey + _productName);
+                var result = key.GetValue(subkeyName);
+                key.Close();
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Write registry entry to HKEY_CURRENT_USER.
+        /// </summary>
+        /// <param name="subkeyName"></param>
+        /// <param name="value"></param>
+        public static void WriteSubKey(string subkeyName, object value)
+        {
+            if (!_initialised) throw new Exception("CTecUtil: the Registry class has not been initialised.");
+
+            try
+            {
+                RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(_rootKey + _productName);
+                key.SetValue(subkeyName, value);  
+                key.Close(); 
+            }
+            catch { }
+        }
+    }
+}
