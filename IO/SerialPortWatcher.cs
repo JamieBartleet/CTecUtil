52968dcf1a0f513a8864bf6bc845aac94d18585c
JamieBartleet <84381974+JamieBartleet@users.noreply.github.com>
2023-07-19 16:28:03 +0100

    230719 save
    230719 save

---
+++ IO/SerialPortWatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTecUtil.IO
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public sealed class SerialPortWatcher : IDisposable
+    {
+    }
+}
+
+https://stackoverflow.com/questions/10550635/new-com-port-available-event
