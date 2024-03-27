b5e70d3b1e78251e7e61321316d9fddde632c152
JamieBartleet <84381974+JamieBartleet@users.noreply.github.com>
2024-03-27 16:58:10 +0000

    240327 save
    240327 save

---
+++ RangeValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTecUtil
+{
+    /// <summary>
+    /// Specifies an integer range object with Min, Max and Default values.
+    /// </summary>
+    public class RangeValue
+    {
+        public RangeValue(int min, int max, int defaultValue) { Min = min; Max = max; Default = defaultValue; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Default { get; set; }
+    }
+}
