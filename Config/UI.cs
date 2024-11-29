4817eeb28bd7c6c35389d29a937a8d37eac58833
JamieBartleet <jamie.bartleet@gmail.com>
2024-11-29 18:39:54 +0000

    241129 save
    241129 save

--- Config/UI.cs
+++ Config/UI.cs
@@ -4,6 +4,11 @@ namespace CTecUtil.Config
 {
     public class UI
     {
+        public static float MinZoom = 0.45f;
+        public static float MaxZoom = 1.25f;
+        public static float ZoomStep => (MaxZoom - MinZoom) / 16;
+
+
         public static ScaleTransform LayoutTransform { get; set; }
     }
 }
