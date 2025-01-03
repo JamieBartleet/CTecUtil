4b0f00f237f9ec426b7371204a15b29c9916475e
JamieBartleet <jamie.bartleet@gmail.com>
2025-01-03 19:09:54 +0000

    250103 v1.5.10 for Quantec v6.1.12 release
    •	Minimum zoom level is not as tiny.

--- TextProcessing.cs
+++ TextProcessing.cs
@@ -369,50 +369,76 @@ namespace CTecUtil
         /// Returns a string representing a range of numbers.  Duplicates are removed and the list sorted.
         /// </summary>
         /// <param name="numberList"></param>
-        /// <returns>Formatted number range string, e.g. "1-5,8,11-15"</returns>
-        public static string NumberListToString(List<int> numberList)
+        /// <param name="useAnd">Inserts '&' before the last item in list</param>
+        /// <returns>Formatted number range string, e.g. "1-5, 8, 11-15"</returns>
+        public static string NumberListToString(List<int> numberList, bool useAnd = false)
         {
             if ((numberList?.Count??0) == 0)
                 return "";
 
             numberList = Ranges.NormaliseIntList(numberList);
 
-            StringBuilder result = new StringBuilder();
-            var seq = 0;
-            
+            //StringBuilder result = new StringBuilder();
+            //var seq = 0;
+            //
+            //for (int i = 0; i < numberList.Count; i++)
+            //{
+            //    if (result.Length > 0)
+            //    {
+            //        if (i > 0)
+            //        {
+            //            if (numberList[i] == numberList[i - 1] + 1)
+            //            {
+            //                if (seq == 0)
+            //                    result.Append("-");
+            //                seq++;
+            //            }
+            //            else if (i > 0)
+            //            {
+            //                if (seq > 0)
+            //                    result.Append(numberList[i - 1]);
+            //                result.Append(", " + numberList[i]);
+            //                seq = 0;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            result.Append(numberList[i]);
+            //        }
+            //    }
+            //    else
+            //        result.Append(numberList[i]);
+            //}
+            //
+            //if (seq > 0)
+            //    result.Append(numberList[^1]);
+
+            StringBuilder temp = new StringBuilder();
             for (int i = 0; i < numberList.Count; i++)
             {
-                if (result.Length > 0)
-                {
-                    if (i > 0)
-                    {
-                        if (numberList[i] == numberList[i - 1] + 1)
-                        {
-                            if (seq == 0)
-                                result.Append("-");
-                            seq++;
-                        }
-                        else if (i > 0)
-                        {
-                            if (seq > 0)
-                                result.Append(numberList[i - 1]);
-                            result.Append("," + numberList[i]);
-                            seq = 0;
-                        }
-                    }
-                    else
-                    {
-                        result.Append(numberList[i]);
-                    }
-                }
-                else
-                    result.Append(numberList[i]);
+                var seqLen = 1;
+                for (int j = i + 1; j < numberList.Count; j++, seqLen++)
+                    if (numberList[j] != numberList[j - 1] + 1)
+                        break;
+
+                var newElement = numberList[i].ToString();
+                if (!temp.ToString().Contains(newElement))
+                    temp.Append((temp.Length > 0 ? ", " : "") + newElement);
+
+                if (seqLen > 2)
+                    temp.Append("-" + numberList[i += seqLen - 1].ToString());
             }
 
-            if (seq > 0)
-                result.Append(numberList[^1]);
+            var result = temp.ToString();
 
-            return result.ToString();
+            if (useAnd)
+            {
+                int comma = 0;
+                if ((comma = result.LastIndexOf(",")) >= 0)
+                    result = result.Substring(0, comma) + " &" + result.Substring(comma + 1);
+            }
+
+            return result;
         }
 
 
